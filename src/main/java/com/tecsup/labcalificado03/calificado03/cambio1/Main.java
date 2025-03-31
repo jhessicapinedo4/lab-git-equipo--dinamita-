ab9710ffb005f2d00efcb0b1a621b0683dcbaeaf
Jhessica Pinedo <jhessicapinedo@gmail.com>
2025-03-31 17:25:09 -0500

    "Cambio 1 de estructura"

--- src/main/java/com/tecsup/labcalificado03/calificado03/cambio1/Main.java
+++
@@ -1,8 +0,0 @@
-package com.tecsup.labcalificado03.calificado03.cambio1;
-
-public class Main {
-	public static void main(String[] args) {
-        System.out.println("¡Hola desde el proyecto Java!");
-
-    }
-}
