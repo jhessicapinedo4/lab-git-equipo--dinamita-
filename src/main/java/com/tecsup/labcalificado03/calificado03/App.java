bd0295e75211d7296d9aed5e48396d5879586d9a
Jhessica Pinedo <jhessicapinedo@gmail.com>
2025-03-31 17:03:18 -0500

    first commit

---
+++ src/main/java/com/tecsup/labcalificado03/calificado03/App.java
@@ -0,0 +1,10 @@
+package com.tecsup.labcalificado03.calificado03;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/tecsup/labcalificado03/calificado03/AppTest.java
@@ -0,0 +1,19 @@
+package com.tecsup.labcalificado03.calificado03;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
