bc253b284da1228a7c59d529401e4f3528e0da54
Luis Alonso Romero <luis.romero.s@tecsup.edu.pe>
2025-03-31 17:46:05 -0500

    add "Añadidas pruebas para funcionalidad4"

--- src/main/java/com/tecsup/labcalificado03/calificado03/test_funcionalidad.java
+++ src/main/java/com/tecsup/labcalificado03/calificado03/test_funcionalidad.java
@@ -3,7 +3,3 @@ package com.tecsup.labcalificado03.calificado03;
 public class test_funcionalidad {
 
 }
-
-from funcionalidad import saludar  
-def test_saludar():  
-    assert saludar("Ana") == "Hola, Ana"  
