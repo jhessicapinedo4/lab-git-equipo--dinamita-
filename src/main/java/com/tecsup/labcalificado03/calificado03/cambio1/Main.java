80407f8473dd859f84eb97210821f3d9b81aa835
Jhessica Pinedo <jhessicapinedo@gmail.com>
2025-03-31 17:21:13 -0500

    "Creada estructura del proyecto"

--- src/main/java/com/tecsup/labcalificado03/calificado03/cambio1/Main.java
+++ src/main/java/com/tecsup/labcalificado03/calificado03/cambio1/Main.java
@@ -1,5 +1,8 @@
 package com.tecsup.labcalificado03.calificado03.cambio1;
 
 public class Main {
+	public static void main(String[] args) {
+        System.out.println("¡Hola desde el proyecto Java!");
 
+    }
 }
